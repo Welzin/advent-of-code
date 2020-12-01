be07ded292f282e83fd9540d2e2b8ced0b47ed20
Johann Rosain <johann.rosain@gmail.com>
2020-12-01 12:40:24 +0100

    Added Day 1 Solutions

---
+++ 1/part1.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+import os
+
+fd = open("input", "r")
+
+entries = []
+
+for line in fd.readlines() :
+    entries.append(int(line))
+
+# Naive - O(n^2) solution :
+for i in range(len(entries)) :
+    for j in range(i, len(entries)) :
+        if entries[i] + entries[j] == 2020 :
+            print("O(n^2) : {}".format(entries[i] * entries[j]))
+            break
+
+# Python's sort algorithm has O(nlog(n)) complexity. The following has O(nlog(n)) complexity. 
+# See also : https://stackoverflow.com/questions/9656789/find-2-numbers-in-an-unsorted-array-equal-to-a-given-sum
+entries.sort()
+st = 0
+ed = len(entries) - 1
+found = False
+
+while not found :
+    sum = entries[st] + entries[ed]
+    if sum == 2020 :
+        found = True
+    elif sum < 2020 :
+        st += 1
+    else :
+        ed -= 1
+
+print("O(nlog(n)) : {}".format(entries[st] * entries[ed]))
---
+++ 1/part2.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import os
+
+fd = open("input", "r")
+
+entries = []
+
+for line in fd.readlines() :
+    entries.append(int(line))
+
+# Naive - O(n^3) solution :
+for i in range(len(entries)) :
+    for j in range(i, len(entries)) :
+        for k in range(j, len(entries)) :
+            if entries[i] + entries[j] + entries[k] == 2020 :
+                print("O(n^3) : {}".format(entries[i] * entries[j] * entries[k]))
+                break
+
+# O(n^2) solution. 
+# See also : https://en.wikipedia.org/wiki/3SUM
+entries.sort()
+
+for i in range(len(entries) - 1) :
+    a = entries[i]
+    st = i + 1
+    ed = len(entries) - 1
+    while st < ed :
+        b = entries[st]
+        c = entries[ed]
+        if a + b + c == 2020 :
+            print("O(n^2) : {}".format(a * b * c))
+            break
+        elif a + b + c > 2020 :
+            ed -= 1
+        else :
+            st += 1
