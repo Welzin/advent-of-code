d63f9c96b1d0bcb28479d6c206be7812cdae00f2
Johann Rosain <johann.rosain@gmail.com>
2020-12-03 10:51:33 +0100

    Added Day 3 Solutions

---
+++ 3/part1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import re
+
+fd = open("input", "r")
+
+col = 0
+count = 0
+
+for line in fd.readlines() :
+    count += line[col % (len(line) - 1)] == '#'
+    col += 3
+
+print(count)
---
+++ 3/part2.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+import re
+
+fd = open("input", "r")
+
+counts = [0] * 5
+cols = [0] * 5
+lines = 0
+adds = [1, 3, 5, 7, 1]
+
+for line in fd.readlines() :
+    for i in range(4) :
+        counts[i] += line[cols[i] % (len(line) - 1)] == '#'
+        cols[i] += adds[i]
+
+    if lines % 2 == 0 :
+        counts[-1] += line[cols[-1] % (len(line) - 1)] == '#'
+        cols[-1] += adds[-1]
+
+    lines += 1
+
+product = 1
+for x in counts :
+    product *= x
+
+print(product)
