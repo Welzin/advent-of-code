4832df5fb92849d71280d61b5513c65cc46431c0
Johann Rosain <johann.rosain@gmail.com>
2021-12-06 13:15:00 +0100

    [AoC2021] Day 6

---
+++ 2021/06/part2.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+def process(population, days) :
+    total = [population.count(i) for i in range(days)]
+    for day in range(days - 7) : total[day + 7] += total[day] + total[day - 2] * (day >= 2)
+    return sum(total) + sum(population.count(i) for i in range(days))
+
+print(process(list(map(int, open("input").readline().split(","))), 256))
