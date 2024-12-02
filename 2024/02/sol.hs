f6dc63a58b194c409c0be23518d61b9e4ac75343
Johann Rosain <johann.rosain@gmail.com>
2024-12-02 12:28:34 +0100

    2024: day 2 better code

--- 2024/02/sol.hs
+++ 2024/02/sol.hs
@@ -14,10 +14,10 @@ unsafe :: Int -> Int -> Int -> Bool
 unsafe x y s =
   let s' = sign x y
       v  = abs $ x - y in
-    (s /= 0 && s' /= 0 && s' /= s) || not(1 <= v && v <= 3)
+    (s /= 0 && s' /= s) || not(1 <= v && v <= 3)
 
 safety :: Int -> [Int] -> Int
-safety _ [_]      = 1
+safety _ [_] = 1
 safety s (x:y:xs)
   | unsafe x y s = 0
   | otherwise = safety (sign x y) (y:xs)
@@ -25,21 +25,16 @@ safety s (x:y:xs)
 part1 :: [[Int]] -> Int
 part1 xs = sum $ map (safety 0) xs
 
-safetyBad :: Int -> Bool -> Int -> Int -> [Int] -> Int
-safetyBad _ _ _ _ [_] = 1
-safetyBad s b z sp (x:y:xs)
-  | not b && unsafe x y s = 0
-  |   b && unsafe x y s = maximum [
-            safetyBad s False z sp (x:xs), -- bad is next
-            safetyBad sp False z sp (z:y:xs), -- bad is head
-            fromEnum (sp /= sign x y) * safetyBad sp False z sp (x:y:xs)
-              -- bad is previous element, being careful not to upset the order
-          ]
-  | otherwise = safetyBad (sign x y) b x s (y:xs)
+safetyBad :: Int -> Int -> Int -> [Int] -> Int
+safetyBad _ _ _ [_] = 1
+safetyBad s z sp (x:y:xs)
+  | unsafe x y s = max (safety s (x:xs))    -- bad is next
+                       (safety sp (z:y:xs)) -- bad is head
+  | otherwise = safetyBad (sign x y) x s (y:xs)
 
 part2 :: [[Int]] -> Int
-part2 xs = sum $ map (\x -> safetyBad 0 True (x!!1 + sign (x!!1) (x!!2)) 0 x) xs
-
+part2 xs = sum $ map (\x -> max (safety 0 (tail x)) $ safetyBad 0 (head x) 0 x) xs
+                      -- Only the head might necessite to remove the "previous" element.
 main :: IO()
 main = do
   args <- getArgs
